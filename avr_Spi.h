2257636d3f2b2e85442d1d4a9fae5ad7cce0f471
DamRCorba <DamRCorba@users.noreply.github.com>
2015-07-10 01:58:57 -0300

    Create avr_Spi.h
    Header file for future Spi Api

---
+++ avr_Spi.h
@@ -0,0 +1,29 @@
+/*
+ * Title: avr_Spi.h Header file for SPI definitions
+ *
+ *  Created on: 8 de jul. de 2015
+ *      Author: Ing. Damian Corbalan
+ *
+ *   LICENCE
+ *   Copyright (C) 2015 Damian Corbalan
+ *
+ *	 This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *
+ *	TODO: avr_spi api
+ */
+
+#ifndef AVR_SPI_H_
+#define AVR_SPI_H_
+
+
+
+#endif /* AVR_SPI_H_ */
