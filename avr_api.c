44c5ec99c6f5c39e6ef45c48446b8d09de86e8f9
DamRCorba <DamRCorba@users.noreply.github.com>
2015-07-10 01:52:47 -0300

    Create avr_api.c

---
+++ avr_api.c
@@ -0,0 +1,24 @@
+/*
+ * Title: avr_api.c
+ *  Created on: 8 de jul. de 2015
+ *      Author: Ing. Damian Corbalan
+ *
+ *
+ *   LICENCE
+ *   Copyright (C) 2015 Damian Corbalan
+ *
+ *	 This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *
+ *
+ */
+
+#include "avr_api.h"
