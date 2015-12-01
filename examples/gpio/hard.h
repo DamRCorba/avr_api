fbf2914ca3a9fd8e618e50fede763a5b485d3add
root <root@Damian.Corbalan>
2015-12-01 17:13:27 -0300

    Actualizacion de nombres de la API en el ejemplo

--- examples/gpio/hard.h
+++ examples/gpio/hard.h
@@ -4,18 +4,18 @@
 #include "../../avr_api/avr_api.h"
 
 
-#define SENS_PORT GPIO_F
+#define SENS_PORT avr_GPIO_F
 
-#define SENS_DER_PIN GPIO_PIN_0 		//Sensor derecho mas alejado
-#define SENS_DERC_PIN GPIO_PIN_1		//Sensor derecho central
-#define SENS_CEN_PIN GPIO_PIN_2			//Sensor central
-#define SENS_IZQC_PIN GPIO_PIN_3		//Sensor izquierdo central
-#define SENS_IZQ_PIN GPIO_PIN_4			//Sensor izquierdo mas alejado
+#define SENS_DER_PIN avr_GPIO_PIN_0 		//Sensor derecho mas alejado
+#define SENS_DERC_PIN avr_GPIO_PIN_1		//Sensor derecho central
+#define SENS_CEN_PIN avr_GPIO_PIN_2			//Sensor central
+#define SENS_IZQC_PIN avr_GPIO_PIN_3		//Sensor izquierdo central
+#define SENS_IZQ_PIN avr_GPIO_PIN_4			//Sensor izquierdo mas alejado
 
-#define SENS_DER GPIOF_OUT_0 		//Sensor derecho mas alejado
-#define SENS_DERC GPIOF_OUT_1		//Sensor derecho central
-#define SENS_CEN GPIOF_OUT_2			//Sensor central
-#define SENS_IZQC GPIOF_OUT_3		//Sensor izquierdo central
-#define SENS_IZQ PINF_4			//Sensor izquierdo mas alejado
+#define SENS_DER avr_GPIOF_OUT_0 		//Sensor derecho mas alejado
+#define SENS_DERC avr_GPIOF_OUT_1		//Sensor derecho central
+#define SENS_CEN avr_GPIOF_OUT_2			//Sensor central
+#define SENS_IZQC avr_GPIOF_OUT_3		//Sensor izquierdo central
+#define SENS_IZQ avr_GPIOF_OUT_4			//Sensor izquierdo mas alejado
 
 #endif /* HARD_H_ */
--- examples/gpio/main.c
+++ examples/gpio/main.c
@@ -6,9 +6,9 @@ int main (void)
 {
 	GpioInitStructure_AVR sens;
 
-	sens.port = SENS_PORT;
-	sens.modo=GPIO_mode_Input;
-	sens.pines=SENS_DER_PIN | SENS_DERC_PIN | SENS_CEN_PIN | SENS_IZQC_PIN | SENS_IZQ_PIN;
+	sens.port =	SENS_PORT;
+	sens.modo = 	avr_GPIO_mode_Input;
+	sens.pine s=	SENS_DER_PIN | SENS_DERC_PIN | SENS_CEN_PIN | SENS_IZQC_PIN | SENS_IZQ_PIN;
 	init_gpio (sens);
 
 	return 0;
